676045bb8c4cda858d07ca022b10516603676e6e
Leonardo Jose Carnieri <lcarnieri@ciandt.com>
2018-01-16 21:18:20 -0200

    [m01] Learning props

--- examples/m01/learning-props/src/app.js
+++ examples/m01/learning-props/src/app.js
@@ -1,16 +1,31 @@
 'use strict'
 
-import React from 'react'
-import Title from './title'
+import React, { Component } from 'react'
+// import Title from './title'
+import Square from './square'
 
-const App = React.createClass({
-  render: function () {
+class App extends Component {
+  render () {
     return (
       <div className='container'>
-        <Title name='Leonardo' />
+        {
+          ['blue', 'red', 'blue'].map((square, key) => (
+            <Square key={key} color={square} />
+          ))
+        }
       </div>
     )
   }
-})
+}
+
+// const App = React.createClass({
+//   render: function () {
+//     return (
+//       <div className='container'>
+//         <Title name='Leonardo' />
+//       </div>
+//     )
+//   }
+// })
 
 export default App
--- examples/m01/learning-props/src/title.js
+++ examples/m01/learning-props/src/title.js
@@ -2,16 +2,51 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps: function () {
-    return {
-      name: 'Desconhecido',
-      lastname: 'Sem sobrenome'
-    }
-  },
-  render: function () {
-    return <h1> Olá {this.props.name + ' ' + this.props.lastname}!</h1>
-  }
-})
+// Usando funções pura.
+// const Title = function () {
+//   return (
+//     <h1>Olá Leonardo</h1>
+//   )
+// }
+
+// Função pura com ECScript6(arrow function)
+// const Title = () => {
+//   return (
+//     <h1>Olá Leonardo</h1>
+//   )
+// }
+
+// const Title = () => <h1>Olá Leonardo</h1>
+
+// OR
+
+// const Title = (props) => (
+//   <h1>Olá {props.name + ' ' + props.lastname}</h1>
+// )
+
+// const Title = (props) => (
+//   <h1>Olá {`${props.name} ${props.lastname}`}</h1>
+// )
+
+const Title = ({name, lastname}) => (
+  <h1>Olá {`${name} ${lastname}!`}</h1>
+)
+
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastname: 'Sem sobrenome'
+}
+
+// const Title = React.createClass({
+//   getDefaultProps: function () {
+//     return {
+//       name: 'Desconhecido',
+//       lastname: 'Sem sobrenome'
+//     }
+//   },
+//   render: function () {
+//     return <h1> Olá {this.props.name + ' ' + this.props.lastname}!</h1>
+//   }
+// })
 
 export default Title
