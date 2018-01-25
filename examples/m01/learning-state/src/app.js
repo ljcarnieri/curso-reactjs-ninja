92a2dc57b904c3987b6611c22ba1d64893f8bf05
Leonardo Jose Carnieri <lcarnieri@ciandt.com>
2018-01-24 22:51:03 -0200

    [m01] Learning states

--- examples/m01/learning-state/src/app.js
+++ examples/m01/learning-state/src/app.js
@@ -1,12 +1,45 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Button from './button'
+import Square from './square'
+import Timer from './timer'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      time: 0,
+      color: 'green',
+      showTimer: true
+    }
+  }
+
   render () {
     return (
-      <div className='container'>
-        My app
+      <div>
+        <Square color={this.state.color} />
+        {['red', 'green', 'blue'].map((color) => (
+          <Button
+            key={color}
+            handlerClick={() => this.setState({ color })}>
+            {color}
+          </Button>
+        ))}
+
+        {this.state.showTimer && <Timer time={this.state.time} />}
+
+        <Button handlerClick={() => (
+          this.setState({ showTimer: !this.state.showTimer })
+        )}>
+          Show / hide timer
+        </Button>
+
+        <Button handlerClick={() => {
+          this.setState({ time: this.state.time + 10 })
+        }}>
+          Change props
+        </Button>
       </div>
     )
   }
---
+++ examples/m01/learning-state/src/timer.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import React, {Component} from 'react'
+
+class Timer extends Component {
+  constructor () {
+    console.log('constructor')
+    super()
+    this.state = {
+      time: 0
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log('componentWillReceiveProps', this.props, nextProps)
+  }
+
+  componentDidMount () {
+    console.log('componentDidMount')
+    this.timer = setInterval(() => {
+      this.setState({ time: this.state.time + 1 })
+    }, 1000)
+  }
+
+  componentWillUnmount () {
+    console.log('componentWillUnmount')
+    clearInterval(this.timer)
+  }
+
+  render () {
+    console.log('render')
+    return <div>Timer: {this.state.time}</div>
+  }
+}
+
+export default Timer
